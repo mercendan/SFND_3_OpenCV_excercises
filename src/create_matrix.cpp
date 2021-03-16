510145ca47830f762f087cb445fbbbeb4c446271
mercendan <mercendan@gmail.com>
2021-03-16 17:50:13 +0100

    Matrix creation impleneted

--- src/create_matrix.cpp
+++ src/create_matrix.cpp
@@ -16,17 +16,26 @@ void createMatrix1()
     // STUDENT TASK :
     // Create a variable of type cv::Mat* named m3_8u which has three channels with a
     // depth of 8bit per channel. Then, set the first channel to 255 and display the result.
+    cv::Mat m3_8u;
+    m3_8u.create(nrows, ncols, CV_8UC3); // multi (three)channel matrix with 8bit unsigned elements
+    m3_8u.setTo(cv::Scalar(255, 0, 0)); //first channel is set to 255
+    //cout << "m3_8u (python)  = " << endl << format(m3_8u, cv::Formatter::FMT_PYTHON) << endl << endl;
+
 
 
     // show result
     string windowName = "First steps in OpenCV (m1_8u)";
     cv::namedWindow(windowName, 1); // create window
     cv::imshow(windowName, m1_8u);
-    cv::waitKey(0); // wait for keyboard input before continuing
+    
 
     // STUDENT TASK :
     // Display the results from the STUDENT TASK above
-
+    // show result
+    string windowName2 = "Display m3_8u";
+    cv::namedWindow(windowName2, 1); // create window
+    cv::imshow(windowName2, m3_8u);
+    cv::waitKey(0); // wait for keyboard input before continuing
 }
 
 
